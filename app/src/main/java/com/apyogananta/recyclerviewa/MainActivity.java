7a571fc581eea89859676836e6aca1ebc388d6f8
DzakySalman <evoyege@gmail.com>
2024-06-07 13:20:24 +0700

    Initial commit

---
+++ app/src/main/java/com/apyogananta/recyclerviewa/MainActivity.java
@@ -0,0 +1,63 @@
+package com.apyogananta.recyclerviewa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private RecyclerView _recyclerView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
+
+        List<NegaraModel> negaraModelList = new ArrayList<>();
+
+        NegaraModel nm = new NegaraModel();
+        nm.setNama("Albania");
+        nm.setDeskripsi("Negara bakpau");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Albania-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Belgium");
+        nm.setDeskripsi("Negara mecyut");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Belgium-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Hungary");
+        nm.setDeskripsi("Negara bakpau2");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Hungary-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Nigeria");
+        nm.setDeskripsi("Negara black");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Nigeria-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Nazi");
+        nm.setDeskripsi("Negara Seniman");
+        nm.setUrl("https://en.wikipedia.org/wiki/Nazi_Germany#/media/File:Flag_of_Germany_(1935%E2%80%931945).svg");
+        negaraModelList.add(nm);
+
+
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+        _recyclerView1.setLayoutManager(lm);
+
+        NegaraAdapter na = new NegaraAdapter(getApplicationContext(), negaraModelList);
+        _recyclerView1.setAdapter(na);
+
+    }
+}
